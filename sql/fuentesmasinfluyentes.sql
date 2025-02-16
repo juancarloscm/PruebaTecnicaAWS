7bd0e5ec095edfb4cb8eb5b6c08ac9081af3407e
juancarloscm <juancarloscm@yahoo.com>
2025-02-15 23:48:44 -0500

    Update fuentesmasinfluyentes.sql

--- sql/fuentesmasinfluyentes.sql
+++ sql/fuentesmasinfluyentes.sql
@@ -1,1 +1,11 @@
+SELECT 
+    source.name AS source_name,
+    COUNT(*) AS total_articles,
+    AVG(source.reliability_score) AS avg_reliability_score
+FROM spaceflight_catalog.fact_article fact_article
+JOIN spaceflight_catalog.dim_news_source source 
+  ON fact_article.source_id = source.source_id
+GROUP BY source.name
+ORDER BY total_articles DESC
+LIMIT 10;
 
