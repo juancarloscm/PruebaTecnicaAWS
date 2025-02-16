22fcca95ddc486abf3bed7103a799215c264f1a3
juancarloscm <juancarloscm@yahoo.com>
2025-02-15 23:47:53 -0500

    Update analisistendencias.sql

--- sql/analisistendencias.sql
+++ sql/analisistendencias.sql
@@ -1,1 +1,10 @@
+SELECT 
+    topic.name AS topic_name,
+    CONCAT(fact_article.year, '-', fact_article.month) AS month,
+    COUNT(*) AS total_articles
+FROM spaceflight_catalog.fact_article fact_article
+JOIN spaceflight_catalog.dim_topic topic 
+  ON fact_article.topic_id = topic.topic_id
+GROUP BY topic.name, fact_article.year, fact_article.month
+ORDER BY month ASC, total_articles DESC;
 
