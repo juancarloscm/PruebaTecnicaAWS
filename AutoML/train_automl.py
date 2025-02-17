929e803e8f43f6f9d0c3b2c53d99693c0de05cec
juancarloscm <juancarloscm@yahoo.com>
2025-02-17 08:01:03 -0500

    Update train_automl.py

--- AutoML/train_automl.py
+++ AutoML/train_automl.py
@@ -1,1 +1,24 @@
+import boto3
+
+sagemaker = boto3.client("sagemaker")
+
+# Configuración del AutoML Job
+automl_job_name = "spaceflight-news-trends"
+input_data = "s3://spaceflight-data-pipeline/training-data/"
+output_data = "s3://spaceflight-data-pipeline/models/"
+
+response = sagemaker.create_auto_ml_job(
+    AutoMLJobName=automl_job_name,
+    InputDataConfig=[{
+        "DataSource": {
+            "S3DataSource": {"S3Uri": input_data, "S3DataType": "S3Prefix"}
+        },
+        "TargetAttributeName": "popularity_score"
+    }],
+    OutputDataConfig={"S3OutputPath": output_data},
+    ProblemType="Regression",
+    AutoMLJobObjective={"MetricName": "MSE"}
+)
+
+print("AutoML Job Iniciado:", response["AutoMLJobArn"])
 
